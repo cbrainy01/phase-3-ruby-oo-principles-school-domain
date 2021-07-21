7bfe7680309a959a88987b40b44f7cff5e92e456
Carl <carlbrainegebe@gmail.com>
2021-07-21 00:09:37 -0500

    commit tree 49cd4748eac297f39f6a0d0bb37fdb4f558a2682

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+class School
+
+    
+    def initialize(name)
+        @school_name = name
+        @roster = {}
+    end
+
+    attr_accessor :school_name
+    attr_accessor :roster
+
+    def add_student(student_name, grade)
+        #if grade already exists, just add the student name to that keys' grades array
+        if @roster[grade] == nil 
+            @roster[grade] = []
+            @roster[grade] << student_name
+        else 
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # roster = {
+        #     9: ["AC Slater", "Zach Morris"],
+        #     10: ["Aardvark", "Kelly Kapowski"]
+        # }
+        # @roster.sort
+        grades = @roster.keys
+        sorted_hash = {}
+        grades.each do |grade|
+            # sorted_hash[grade] = []
+            sorted_hash[grade] = @roster[grade].sort
+        end
+        sorted_hash
+    end
+end
+#    @roster[grade] ||= []
+#         @roster[grade] << student_name
